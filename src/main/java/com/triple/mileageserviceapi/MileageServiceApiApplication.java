94d6bc25aba00756d965154c8aeda8663b1bb20e
min cho <minnaldo6602@gmail.com>
2022-07-02 20:57:57 +0900

    Initial commit

---
+++ src/main/java/com/triple/mileageserviceapi/MileageServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.triple.mileageserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MileageServiceApiApplication.class, args);
+	}
+
+}
